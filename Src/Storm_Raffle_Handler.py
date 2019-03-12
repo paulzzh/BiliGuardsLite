3869efb0f82ad66977aab835b1818fbf006d832c
TheWanderingCoel <39617891+TheWanderingCoel@users.noreply.github.com>
2019-03-12 18:17:03 +0800

    DEBUG STORM

--- Src/Storm_Raffle_Handler.py
+++ Src/Storm_Raffle_Handler.py
@@ -36,15 +36,24 @@ class StormRaffleHandler:
                 Log.error("4")
                 Timer.add2list_jobs(StormRaffleHandler.join,time_wanted,(room_id,raffle_id))
                 Log.error("5")
-
+    
     @staticmethod
     async def join(room_id,raffle_id):
+        Log.error("6")
         await BasicRequest.enter_room(room_id)
+        Log.error("7")
         data = await BasicRequest.storm_req_join(raffle_id)
+        Log.error("8")
         Statistics.add2joined_raffles("节奏风暴(合计)")
+        Log.error("9")
         if not data["code"]:
+            Log.error("10")
             data = data["data"]
+            Log.error("11")
             gift_name = data["gift_name"]
+            Log.error("12")
             gift_num = data["gift_num"]
+            Log.error("13")
             Log.critical("房间 %s 节奏风暴抽奖结果: %s X %s")
+            Log.error("14")
             Statistics.add2results(gift_name,int(gift_num))
