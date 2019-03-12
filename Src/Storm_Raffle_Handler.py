1435712afe26a27c4738c30e92e04eccbecf421b
TheWanderingCoel <39617891+TheWanderingCoel@users.noreply.github.com>
2019-03-12 12:29:31 +0800

    DEBUG节奏风暴

--- Src/Storm_Raffle_Handler.py
+++ Src/Storm_Raffle_Handler.py
@@ -27,9 +27,13 @@ class StormRaffleHandler:
             raffle_id = data["id"]
             if not Statistics.is_raffleid_duplicate(raffle_id):
                 Log.info("本次获取到 二十倍节奏风暴 的抽奖id为 %s"%raffle_id)
+                print(1)
                 list_available_raffleid.append((raffle_id,0))
+                print(2)
                 Statistics.add2raffle_ids(raffle_id)
+                print(3)
             for raffle_id,time_wanted in list_available_raffleid:
+                print(4)
                 Timer.add2list_jobs(StormRaffleHandler.join,time_wanted,(room_id,raffle_id))
     
     @staticmethod
